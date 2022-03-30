4c3bd8abd5925555d05888a598a5d2ca3dce272c
emngheo174 <50536185+emngheo174@users.noreply.github.com>
2022-03-30 10:35:30 +0700

    Update ProductDAO.php

--- dao/ProductDAO.php
+++ dao/ProductDAO.php
@@ -5,49 +5,4 @@ require('C:\xampp\htdocs\Baitap\OOP\OOP\abstract\BaseDao.php');
 
 class ProductDao extends BaseDao
 {
-    /**
-     * Insert row to productTable
-     * 
-     * @param $row
-     * @return array
-     */
-    public function insert($row)
-    {
-        $this->database->insertTable('productTable', $row);
-        print_r($this->database->selectTable('productTable'));
-    }
-
-    /**
-     * Select row to Product table
-     * 
-     * @param $row
-     * @return array
-     */
-    public function select($row)
-    {
-        return $this->database->selectTable('productTable', $row);
-    }
-
-    /**
-     * Update row to Product table
-     * 
-     * @param $row
-     * @return array
-     */
-    public function update($row)
-    {
-        return $this->database->updateTable('productTable', $row);
-    }
-
-    /**
-     * Delete row to Product table
-     * 
-     * @param $row
-     * @return void
-     */
-    public function delete($row)
-    {
-        return $this->database->deleteTable('productTable', $row);
-    }
-
 }
